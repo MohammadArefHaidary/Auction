d21e9534dbcdb332a72314e97673b3a0b81df38b
Mohammad Aref <mohammadaref.haidary1234@gmail.com>
2022-05-30 15:19:41 +0100

    fix:style4

--- js.js
+++ js.js
@@ -62,45 +62,4 @@ ups.forEach(clickedTab =>{
 	}
 
 
-	// timer
-	function countdownTimeStart(){
-
-		var countDownDate = new Date("Sep 25, 2025 15:00:00").getTime();
-		
-		// Update the count down every 1 second
-		var x = setInterval(function() {
-		
-			// Get todays date and time
-			var now = new Date().getTime();
-			
-			// Find the distance between now an the count down date
-			var distance = countDownDate - now;
-			
-			// Time calculations for days, hours, minutes and seconds
-			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-			
-			// Output the result in an element with id="demo"
-			document.getElementById("demo").innerHTML = hours + "h "
-			+ minutes + "m " + seconds + "s ";
-			
-			// If the count down is over, write some text 
-			if (distance < 0) {
-				clearInterval(x);
-				document.getElementById("demo").innerHTML = "EXPIRED";
-			}
-		});
-		}
-
-
-		const btn = document.getElementById('bbttnn');
-
-btn.addEventListener('click', () => {
-  // 👇️ hide button
-  btn.style.display = 'none';
-
-  // 👇️ show div
-//   const box = document.getElementById('box');
-//   box.style.display = 'block';
-});
+	
